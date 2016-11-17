51059566302b3cd817731529acbfcf2f74ddbd02
rookwood101 <alexandersadler1@gmail.com>
2016-11-17 16:12:21 +0000

    Begin implementing BreadthFirstSearcher Implement Node struct

--- src/blocksworld/search.rs
+++ src/blocksworld/search.rs
@@ -1,3 +1,24 @@
+use ::blocksworld::world;
+
 struct BreadthFirstSearcher {
-    
+    start_state: world::World,
+    goal_state: world::World,
+}
+
+impl BreadthFirstSearcher {
+    pub fn new(start_state: world::World, goal_state: world::World) -> BreadthFirstSearcher {
+        BreadthFirstSearcher {
+            start_state: start_state,
+            goal_state: goal_state,
+        }
+    }
+    pub fn search<'a>() -> Node<'a> {
+        unimplemented!();
+    }
+}
+
+struct Node<'a> {
+    depth: u64,
+    world: Box<world::World>,
+    parent: Option<&'a Node<'a>>,
 }
