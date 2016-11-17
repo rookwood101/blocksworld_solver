43b29e4a280fde6dcdba33c365c2673c2f5720d2
rookwood101 <alexandersadler1@gmail.com>
2016-11-17 23:55:07 +0000

    Implement hard and easy search problems

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 mod blocksworld;
 
 use blocksworld::world::*;
+use blocksworld::search::*;
 use std::collections::HashMap;
 
 fn main() {
-    let mut entity_starts = HashMap::new();
-    entity_starts.insert(Entity::Block('A'), Location::new(0, 3));
-    entity_starts.insert(Entity::Block('B'), Location::new(1, 3));
-    entity_starts.insert(Entity::Block('C'), Location::new(2, 3));
-    entity_starts.insert(Entity::Agent, Location::new(3, 3));
+    let mut hard_entity_start_positions = HashMap::new();
+    hard_entity_start_positions.insert(Entity::Block('A'), Location::new(0, 3));
+    hard_entity_start_positions.insert(Entity::Block('B'), Location::new(1, 3));
+    hard_entity_start_positions.insert(Entity::Block('C'), Location::new(2, 3));
+    hard_entity_start_positions.insert(Entity::Agent, Location::new(3, 3));
 
-    let grid = World::new(4, 4, &entity_starts).unwrap();
-    grid.pretty_print();
-    let grid2 = grid.clone_and_move_agent(Direction::Up).unwrap();
-    grid2.pretty_print();
+    let mut easy_entity_start_positions = HashMap::new();
+    easy_entity_start_positions.insert(Entity::Block('A'), Location::new(1, 1));
+    easy_entity_start_positions.insert(Entity::Block('B'), Location::new(1, 2));
+    easy_entity_start_positions.insert(Entity::Block('C'), Location::new(0, 3));
+    easy_entity_start_positions.insert(Entity::Agent, Location::new(2, 3));
+
+    let mut entity_goal_positions = HashMap::new();
+    entity_goal_positions.insert(Entity::Block('A'), Location::new(1, 1));
+    entity_goal_positions.insert(Entity::Block('B'), Location::new(1, 2));
+    entity_goal_positions.insert(Entity::Block('C'), Location::new(1, 3));
+    entity_goal_positions.insert(Entity::Agent, Location::new(3, 3));
+
+    let start_world = World::new(4, 4, &easy_entity_start_positions).unwrap();
+    let goal_world = World::new(4, 4, &entity_goal_positions).unwrap();
+
+    println!("Beginning Search!");
+
+    let searcher = BreadthFirstSearcher::new(start_world, goal_world);
+    let goal_node = searcher.search();
+    goal_node.print_tree();
 }
