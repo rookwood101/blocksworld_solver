d346456864246932fb6921fa69a455088a240abc
rookwood101 <alexandersadler1@gmail.com>
2016-11-23 17:14:48 +0000

    Make A* tree search not graph search

--- src/blocksworld/search/a_star_searcher.rs
+++ src/blocksworld/search/a_star_searcher.rs
@@ -1,8 +1,5 @@
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
-use std::collections::HashSet;
-use std::hash::Hash;
-use std::hash::Hasher;
 use std::cmp::Ordering;
 use std::rc::Rc;
 
@@ -15,7 +12,6 @@ pub struct AStarSearcher {
     goal_world: world::World,
     goal_block_locations: HashMap<world::Entity, world::Location>,
     fringe: BinaryHeap<AStarNode>,
-    explored_states: HashSet<world::World>, // not completely necessary
 }
 impl AStarSearcher {
     pub fn new(start_world: world::World, goal_world: world::World) -> AStarSearcher {
@@ -34,7 +30,6 @@ impl AStarSearcher {
             goal_world: goal_world,
             goal_block_locations: goal_block_locations,
             fringe: BinaryHeap::new(),
-            explored_states: HashSet::new(),
         }
     }
     pub fn search(&mut self) -> AStarNode {
@@ -56,9 +51,6 @@ impl AStarSearcher {
         }
         heuristic
     }
-    fn is_node_previously_visited(&self, node: &AStarNode) -> bool {
-        self.explored_states.contains(node.get_world())
-    }
     fn is_node_unoptimal(&self, node: &AStarNode) -> bool {
         self.fringe
             .iter()
@@ -75,19 +67,13 @@ impl Searcher for AStarSearcher {
         &self.goal_world
     }
     fn fringe_push(&mut self, node: Self::NodeType) {
-        if self.is_node_previously_visited(&node) || self.is_node_unoptimal(&node) {
+        if self.is_node_unoptimal(&node) {
             return;
         }
         self.fringe.push(node);
     }
     fn fringe_pop(&mut self) -> Option<Self::NodeType> {
-        match self.fringe.pop() {
-            Some(next) => {
-                self.explored_states.insert(next.get_world().clone());
-                Some(next)
-            }
-            None => None,
-        }
+        self.fringe.pop()
     }
     fn new_node(&self,
                 depth: u64,
@@ -156,10 +142,5 @@ impl PartialOrd for AStarNode {
 impl Ord for AStarNode {
     fn cmp(&self, other: &AStarNode) -> Ordering {
         other.partial_cmp(self).unwrap()
-    }
-}
-impl Hash for AStarNode {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.get_world().hash(state)
     }
 }
